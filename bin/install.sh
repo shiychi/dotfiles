2ae740cf2904d20ef192fea61aa7bbd50b43be01
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-20 19:09:36 +0900

    :sparkles: add installation Homebrew

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DOTPATH=~/dotfiles/
+DOTURL=https://github.com/mtiyz/dotfiles.git
+
 function choise {
   while true; do
     echo -n "$1 [Y/n]: "
@@ -16,7 +19,27 @@ function choise {
 }
 
 if choise "Do you want to start install"; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  if type "apt" > /dev/null 2>&1; then
+    sudo apt install build-essential curl file git
+  fi
+  if type "yum" > /dev/null 2>&1; then
+    sudo yum groupinstall 'Development Tools'
+    sudo yum install curl file git
+    sudo yum install libxcrypt-compat
+  fi
+  if type "git" > /dev/null 2>&1; then
+    if [ -d $DOTPATH ]; then
+      cd $DOTPATH && git pull
+    else
+      git clone $DOTURL $DOTPATH
+    fi
+  else
+    echo "Require git"
+    exit 1
+  fi
+  cd $DOTPATH
+  /bin/bash ./homebrew/install.sh
 else
-  echo "Quit" && exit 0
+  echo "Quit"
+  exit 0
 fi
