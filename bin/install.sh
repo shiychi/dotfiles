3c8a229fef25bdbdc5dfb65908a42225858f2da2
noroz <54768914+norozic@users.noreply.github.com>
2020-09-21 01:39:36 +0900

    🐛 fix install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-DOTPATH=~/dotfiles DOTURL=https://github.com/norozic/dotfiles.git if type "apt" > /dev/null 2>&1; then if type "sudo" > /dev/null 2>&1; then :
+DOTPATH=~/dotfiles
+DOTURL=https://github.com/norozic/dotfiles.git
+
+if type "apt" > /dev/null 2>&1; then if type "sudo" > /dev/null 2>&1; then :
   else
     apt install sudo -y
   fi
